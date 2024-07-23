cbba7d1733b4e39dd9778c2001113d44d19afa67
paul_ <pavel.mordukhai@gmail.com>
2024-07-23 23:30:26 +0700

    KS-01-02 solution

--- src/main/kotlin/lesson_01/lesson_01_task_02.kt
+++ src/main/kotlin/lesson_01/lesson_01_task_02.kt
@@ -1,6 +1,7 @@
 package org.example.lesson_01
 
 fun main() {
+
     var numberOfOrders: UInt = 75u
     val thanksForBuying: String = "Спасибо за покупку! Ждём вас в будущем!"
     var numberOfEmployees: UInt = 2000u
