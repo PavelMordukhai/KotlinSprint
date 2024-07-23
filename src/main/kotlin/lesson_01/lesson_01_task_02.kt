69be2a76bd4472c4e68f98c4f4035294aafb858d
paul_ <pavel.mordukhai@gmail.com>
2024-07-23 23:17:08 +0700

    KS-01-03 solution

--- src/main/kotlin/lesson_01/lesson_01_task_02.kt
+++ src/main/kotlin/lesson_01/lesson_01_task_02.kt
@@ -1,6 +1,15 @@
 package org.example.lesson_01
 
 fun main() {
-    println("НИЧОСИ")
-    println("НИЧОСИ")
+    var numberOfOrders: UInt = 75u
+    val thanksForBuying: String = "Спасибо за покупку! Ждём вас в будущем!"
+    var numberOfEmployees: UInt = 2000u
+
+    println(numberOfOrders)
+    println(thanksForBuying)
+
+    // println(numberOfEmployees)
+
+    numberOfEmployees = 1999u
+    println(numberOfEmployees)
 }
