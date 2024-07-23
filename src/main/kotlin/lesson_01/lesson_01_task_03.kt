2d75edcbd597bd5ab14e3401213b92e733163a34
paul_ <pavel.mordukhai@gmail.com>
2024-07-23 23:29:08 +0700

    KS-01-03 solution

--- src/main/kotlin/lesson_01/lesson_01_task_03.kt
+++ src/main/kotlin/lesson_01/lesson_01_task_03.kt
@@ -1,6 +1,16 @@
 package org.example.lesson_01
 
 fun main() {
-    println("НИЧОСИ")
-    println("НИЧОСИ")
+    val year: Short = 1961
+    var hour: Byte = 9
+    var minute: Byte = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
 }
