dabc5fe1735df1f1330df529da133dc9e3e0d8fe
paul_ <pavel.mordukhai@gmail.com>
2024-07-24 21:39:54 +0700

    KS-01-05 new solution

--- src/main/kotlin/lesson_01/lesson_01_task_05.kt
+++ src/main/kotlin/lesson_01/lesson_01_task_05.kt
@@ -5,10 +5,13 @@ fun main() {
     // количество секунд, которые Гагарин провел в космосе
     val seconds: Short = 6480
 
-    val hour = seconds / 3600
-    val minute = (seconds % 3600) / 60
-    val second = seconds % 60
+    val hour = seconds / SECONDS_IN_AN_HOUR
+    val minute = (seconds % SECONDS_IN_AN_HOUR) / SECONDS_IN_AN_MINUTE
+    val second = seconds % SECONDS_IN_AN_MINUTE
 
     // println("0$hour:$minute:0$second")
     println(String.format("%02d:%02d:%02d", hour, minute, second))
 }
+
+const val SECONDS_IN_AN_HOUR = 3600
+const val SECONDS_IN_AN_MINUTE = 60
