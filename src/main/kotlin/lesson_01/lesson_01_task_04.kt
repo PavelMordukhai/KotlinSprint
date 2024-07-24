3a0f874170455408c04b90ed0f98fd29045f4bc4
paul_ <pavel.mordukhai@gmail.com>
2024-07-24 08:24:40 +0700

    KS-01-04 solution

---
+++ src/main/kotlin/lesson_01/lesson_01_task_04.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_01
+
+fun main() {
+
+    //создание переменных
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    // вывод переменных построчно
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
