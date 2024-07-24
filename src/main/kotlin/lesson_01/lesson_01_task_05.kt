9e2b6876b9b21221da66e9d0cade4efc00a5dba9
paul_ <pavel.mordukhai@gmail.com>
2024-07-24 21:42:05 +0700

    KS-01-05 new solution

--- src/main/kotlin/lesson_01/lesson_01_task_05.kt
+++ src/main/kotlin/lesson_01/lesson_01_task_05.kt
@@ -6,12 +6,12 @@ fun main() {
     val seconds: Short = 6480
 
     val hour = seconds / SECONDS_IN_AN_HOUR
-    val minute = (seconds % SECONDS_IN_AN_HOUR) / SECONDS_IN_AN_MINUTE
-    val second = seconds % SECONDS_IN_AN_MINUTE
+    val minute = (seconds % SECONDS_IN_AN_HOUR) / SECONDS_IN_A_MINUTE
+    val second = seconds % SECONDS_IN_A_MINUTE
 
     // println("0$hour:$minute:0$second")
     println(String.format("%02d:%02d:%02d", hour, minute, second))
 }
 
 const val SECONDS_IN_AN_HOUR = 3600
-const val SECONDS_IN_AN_MINUTE = 60
+const val SECONDS_IN_A_MINUTE = 60
