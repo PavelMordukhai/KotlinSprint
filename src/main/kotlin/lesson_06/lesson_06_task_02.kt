cf6c9bcdccd1a200d977f226cb68f8581f29bb81
paul_ <pavel.mordukhai@gmail.com>
2024-08-06 16:03:15 +0700

    KS-06-02 без цикла

--- src/main/kotlin/lesson_06/lesson_06_task_02.kt
+++ src/main/kotlin/lesson_06/lesson_06_task_02.kt
@@ -2,13 +2,8 @@ package org.example.lesson_06
 
 fun main() {
     print("Введите количество секунд, которые нужно засечь: ")
-    val seconds = readln().toInt()
-    var secondsLeft = seconds
+    val seconds = readln().toLong()
+    Thread.sleep(seconds * 1000)
 
-    while (secondsLeft > 0) {
-        secondsLeft--
-        Thread.sleep(1000)
-    }
     println("Прошло $seconds секунд")
 }
-
