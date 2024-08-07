dc6399a95740f7dcf737d13ea09b3174164db8fa
paul_ <pavel.mordukhai@gmail.com>
2024-08-07 08:15:33 +0700

    KS-06-05

---
+++ src/main/kotlin/lesson_06/lesson_06_task_05.kt
@@ -0,0 +1,50 @@
+package org.example.lesson_06
+
+import kotlin.random.Random
+
+fun main() {
+    println("Регистрация нового аккаунта\n")
+    print("Придумайте логин: ")
+    val newUserName = readln()
+    print("Придумайте надёжный пароль: ")
+    val newPassword = readln()
+    println()
+
+    var tries = TRIES
+    println("Докажите, что вы не бот. Число попыток - ${tries}.\n")
+
+    while (tries > 0) {
+        val firstVal = Random.nextInt(MIN_NUM, MAX_NUM + 1)
+        val secondVal = Random.nextInt(MIN_NUM, MAX_NUM + 1)
+
+        print("$firstVal + $secondVal = ")
+        val answer = readln().toInt()
+
+        if (answer == firstVal + secondVal) {
+            println("\nДобро пожаловать!")
+
+            var count = 0
+            do {
+                if (count++ > 0) println("\nНеверный ввод")
+
+                print("\nВведите логин: ")
+                val userName = readln()
+                print("Введите пароль: ")
+                val password = readln()
+            } while ((userName != newUserName) || (password != newPassword))
+
+            println("\nАвторизация прошла успешно")
+            break
+
+        } else if (--tries > 0) println("\nПопробуйте ещё раз")
+    }
+
+    if (tries == 0) {
+        println("\nДоступ запрещен")
+        return
+    }
+}
+
+const val MIN_NUM = 1
+const val MAX_NUM = 9
+const val TRIES = 3
