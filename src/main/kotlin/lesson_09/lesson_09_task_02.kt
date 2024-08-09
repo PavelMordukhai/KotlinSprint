235093aee0fff993d206b1b86402c1ecc9fcff2d
paul_ <pavel.mordukhai@gmail.com>
2024-08-09 10:35:56 +0700

    KS-09-02

---
+++ src/main/kotlin/lesson_09/lesson_09_task_02.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_09
+
+fun main() {
+
+    val ingredientsList = mutableListOf("яйца", "соль", "растительное масло")
+
+    println("В рецепте есть базовые ингредиенты: ")
+    ingredientsList.forEach { ingredient: String ->
+        println("- $ingredient")
+    }
+
+    print("\nЖелаете добавить еще? (да / нет) ")
+    val answer = readln()
+
+    if (answer == "да") {
+        print("Какой ингредиент вы хотите добавить? ")
+        val newIngredient = readln()
+        ingredientsList.add(newIngredient)
+    } else return
+
+    println("\nТеперь в рецепте есть следующие ингредиенты: ")
+    ingredientsList.forEach { ingredient: String ->
+        println("- $ingredient")
+    }
+}
