0f6cadf8c70e2bfe57f83bf606b82e6cf997b9c9
paul_ <pavel.mordukhai@gmail.com>
2024-08-14 22:00:34 +0700

    KS-13-01 PhoneBookContact исправлено свойство company

--- src/main/kotlin/lesson_13/lesson_13_task_01.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_01.kt
@@ -4,10 +4,5 @@ package org.example.lesson_13
 class PhoneBookContact(
     val name: String,
     val phoneNumber: Long,
-) {
-    constructor(
-        name: String,
-        phoneNumber: Long,
-        company: String?,
-    ) : this(name, phoneNumber)
-}
+    val company: String? = null
+)
