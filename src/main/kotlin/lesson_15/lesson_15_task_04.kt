3e0f3ba5329adb584657d34c172be1fbbf4dc0e7
paul_ <pavel.mordukhai@gmail.com>
2024-08-18 12:17:16 +0700

    KS-15-04 удалён лишний код

--- src/main/kotlin/lesson_15/lesson_15_task_04.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_04.kt
@@ -2,7 +2,7 @@ package org.example.lesson_15
 
 
 interface ProductSearch {
-    fun searchProduct(component: Product)
+    fun searchProduct()
 }
 
 
@@ -23,34 +23,14 @@ class MusicalInstrument(
     number: Int,
 ) : Product(name, number), ProductSearch {
 
-    private val numberOfInstr = number
-    private val instrumentName = name
-
-    override fun searchProduct(component: Product) {
-
-        println("Выполняется поиск\n")
-        println("Для инструмента \"$instrumentName\" выполняется поиск компонента \"${component.name}\"")
-
-        if (component.number >= numberOfInstr)
-            println("Найдены компоненты для всех инструментов")
-        else {
-            val result = numberOfInstr - component.number
-
-            println("Не найдены компоненты для инструментов в количестве $result шт.")
-        }
+    override fun searchProduct() {
+        println("Выполняется поиск")
     }
 }
 
 
 fun main() {
 
-    val component1 = MusicalInstrumentComponent("смычок", 5)
     val instrument1 = MusicalInstrument("скрипка", 8)
-
-    val component2 = MusicalInstrumentComponent("чехол", 9)
-    val instrument2 = MusicalInstrument("гитара", 8)
-
-    instrument1.searchProduct(component1)
-    println()
-    instrument2.searchProduct(component2)
+    instrument1.searchProduct()
 }
