99b53c5a1685e5c8118730eeaf1f05a8c312894c
paul_ <pavel.mordukhai@gmail.com>
2024-08-20 10:23:45 +0700

    KS-16-04 исправления в приватности new

--- src/main/kotlin/lesson_16/lesson_16_task_04.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_04.kt
@@ -24,9 +24,9 @@ fun main() {
 
     val order1 = Order(1)
 
-    var statusOfOrder1 = order1.applyFor("В сборе")
+    order1.applyFor("В сборе")
     println("Заказ №${order1.getNumberOfOrder()}: ${order1.getReadinessStatus()}")
 
-    statusOfOrder1 = order1.applyFor("Готов")
+    order1.applyFor("Готов")
     println("Заказ №${order1.getNumberOfOrder()}: ${order1.getReadinessStatus()}")
 }
