7637b6059073cbd3d1cdf125783472f50a0ee099
paul_ <pavel.mordukhai@gmail.com>
2024-08-23 08:29:22 +0700

    KS-19-03 использование TODO

---
+++ src/main/kotlin/lesson_19/lesson_19_task_03.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_19
+
+class Spaceship {
+
+    fun takeOff() {
+        TODO("Здесь нужна информация о дополнительной логике")
+    }
+
+    fun land() {
+        TODO()
+    }
+
+    fun shoot() {
+        println("shooting")
+    }
+}
+
+fun main() {
+    val spaceship = Spaceship()
+    spaceship.takeOff()
+}
