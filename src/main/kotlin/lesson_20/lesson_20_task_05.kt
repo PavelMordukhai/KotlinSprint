8fa4b9d7f9378e33bae618c3f8b6a37b14b847b2
paul_ <pavel.mordukhai@gmail.com>
2024-08-26 17:31:51 +0700

    KS-20-05 модификатор речи - исправления

--- src/main/kotlin/lesson_20/lesson_20_task_05.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_05.kt
@@ -1,26 +1,29 @@
 package org.example.lesson_20
 
-enum class RobotPhrases(val phrase: String) {
-    PHRASE_1("Я кибернетический организм"),
-    PHRASE_2("Пока моя миссия не завершена, я должен функционировать"),
-    PHRASE_3("У меня есть подробные данные о человеческой анатомии"),
-    PHRASE_4("Моя батарейка рассчитана на 120 лет"),
-    PHRASE_5("У меня нейросетевой центральный процессор"),
+class Robot {
+    private val robotPhrases = listOf(
+        "Я кибернетический организм",
+        "Пока моя миссия не завершена, я должен функционировать",
+        "У меня есть подробные данные о человеческой анатомии",
+        "Моя батарейка рассчитана на 120 лет",
+        "У меня нейросетевой центральный процессор"
+    )
+
+    fun say(mod: ((String) -> String)? = null) {
+        var text = robotPhrases.random()
+        if (mod != null)
+            text = mod(text)
+        println(text)
+    }
+
+    fun setModifier(stringModifier: (String) -> String) = stringModifier
 }
 
 fun main() {
+    val robot = Robot()
     val reversedString: (String) -> String = { it.reversed() }
 
-    say()
-    val mod = setModifier(reversedString)
-    say(mod)
+    robot.say()
+    val reversedStringMod = robot.setModifier(reversedString)
+    robot.say(reversedStringMod)
 }
-
-fun say(mod: ((String) -> String)? = null) {
-    var text = RobotPhrases.entries.random().phrase
-    if (mod != null)
-        text = mod(text)
-    println(text)
-}
-
-fun setModifier(stringModifier: (String) -> String) = stringModifier
