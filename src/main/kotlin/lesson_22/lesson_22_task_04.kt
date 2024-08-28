f346f1044f0e9faaba62e75e7ab1ad230c51035d
paul_ <pavel.mordukhai@gmail.com>
2024-08-28 10:02:57 +0700

    KS-22-04 имитация процесса загрузки данных new2

--- src/main/kotlin/lesson_22/lesson_22_task_04.kt
+++ src/main/kotlin/lesson_22/lesson_22_task_04.kt
@@ -1,39 +1,18 @@
 package org.example.lesson_22
 
-class ViewModel {
+class ViewModel(val mainScreenState: MainScreenState) {
+
     data class MainScreenState(
         val data: String? = null,
         val isLoading: Boolean = false,
-    ) {
-        fun loadData(state: MainScreenState): MainScreenState {
-            return if (state.data == null)
-                state.copy(data = null, isLoading = false)
-            else if (!state.isLoading)
-                state.copy(isLoading = true)
-            else
-                state.copy(isLoading = false)
-        }
+    )
+
+    fun loadData(): MainScreenState {
+        return if (mainScreenState.data == null)
+            mainScreenState.copy(data = null, isLoading = false)
+        else if (!mainScreenState.isLoading)
+            mainScreenState.copy(isLoading = true)
+        else
+            mainScreenState.copy(isLoading = false)
     }
-}
-
-fun main() {
-
-    val state = ViewModel.MainScreenState()
-    println(state)
-    println(state.loadData(state))
-    println()
-
-    val state2 = ViewModel.MainScreenState(isLoading = true)
-    println(state2)
-    println(state2.loadData(state2))
-    println()
-
-    val state3 = ViewModel.MainScreenState("some data")
-    println(state3)
-    println(state3.loadData(state3))
-    println()
-
-    val state4 = ViewModel.MainScreenState("some data", true)
-    println(state4)
-    println(state4.loadData(state4))
 }
